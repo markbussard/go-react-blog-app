ed1dfce1d266693bae08d2e3c510f9df70302f1c
markbussard <markbussard@outlook.com>
2024-03-04 23:01:26 -0800

    Update likes schema Add likeable_type and update post_id to likeable_id. Makes this table reusable for likes associated with posts and comments. Also removed updated_at timestamp column

--- backend/sql/schema/005_like.sql
+++ backend/sql/schema/005_like.sql
@@ -1,10 +1,15 @@
 -- +goose Up
+CREATE TYPE likeable_type AS ENUM (
+  'POST',
+  'COMMENT'
+);
+
 CREATE TABLE "like" (
-  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id uuid NOT NULL REFERENCES "user"(id),
-  post_id uuid NOT NULL REFERENCES post(id),
-  created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now()
+  likeable_id uuid NOT NULL,
+  likeable_type likeable_type NOT NULL,
+  created_at timestamp NOT NULL DEFAULT now()
 );
 
 -- +goose Down
