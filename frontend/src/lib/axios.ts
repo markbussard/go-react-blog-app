4f9b632e20b198d32ebb607e92333171d63b7d0c
markbussard <markbussard@outlook.com>
2024-03-04 23:11:59 -0800

    Add axios authRequestInterceptor

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,28 @@
-import Axios from "axios";
+import Axios, { type InternalAxiosRequestConfig } from "axios";
+
+import { API_URL } from "~/config";
+import { firebaseAuth } from ".";
+
+async function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  const firebaseUser = firebaseAuth.currentUser;
+  if (firebaseUser && config.headers) {
+    const jwtToken = await firebaseUser.getIdToken();
+    config.headers.authorization = `${jwtToken}`;
+  }
+  return config;
+}
 
 export const axios = Axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: API_URL,
 });
+
+axios.interceptors.request.use(authRequestInterceptor);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
