bd22bba237ec596e2fcf9eb5e2b8eda524f9db96
markbussard <markbussard@outlook.com>
2024-03-04 23:05:55 -0800

    Update routes entry with new routes and middlewares Add built in chi middlewares for request id, real ip, logger, recover, and heartbeat. Also add LoadUser for all routes and added additional routes for new crud / api functionality

--- backend/server/routes.go
+++ backend/server/routes.go
@@ -2,12 +2,20 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/markbussard/go-react-blog-app/server/handlers"
 )
 
 func (srv *server) ConfigureRouter() {
 	srv.router = chi.NewRouter()
+
+	srv.router.Use(middleware.RequestID)
+	srv.router.Use(middleware.RealIP)
+	srv.router.Use(middleware.Logger)
+	srv.router.Use(middleware.Recoverer)
+
+	srv.router.Use(middleware.Heartbeat("/ping"))
 
 	srv.router.Use(cors.Handler(
 		cors.Options{
@@ -20,13 +28,25 @@ func (srv *server) ConfigureRouter() {
 		},
 	))
 
-	srv.router.Get("/health", handlers.Health)
-
 	apiRouter := chi.NewRouter()
+
+	apiRouter.Use(srv.LoadUser)
 
 	apiRouter.Get("/users/me", srv.withUserAndEnv(handlers.GetMe))
 	apiRouter.Post("/users", srv.withEnv(handlers.CreateUser))
+
 	apiRouter.Get("/posts", srv.withEnv(handlers.GetPosts))
+	apiRouter.Get("/posts/{slug}", srv.withEnv(handlers.GetPostBySlug))
+
+	apiRouter.Get("/posts/{slug}/comments", srv.withEnv(handlers.GetCommentsByPostSlug))
+	apiRouter.Post("/posts/{slug}/comments", srv.withUserAndEnv(handlers.CreateComment))
+	apiRouter.Patch("/posts/{slug}/comments/{commentID}", srv.withUserAndEnv(handlers.UpdateComment))
+	apiRouter.Delete("/posts/{slug}/comments/{commentID}", srv.withUserAndEnv(handlers.DeleteComment))
+
+	apiRouter.Post("/posts/{slug}/likes", srv.withUserAndEnv(handlers.CreatePostLike))
+	apiRouter.Post("/posts/{slug}/comments/{commentID}/likes", srv.withUserAndEnv(handlers.CreateCommentLike))
+	apiRouter.Delete("/posts/{slug}/comments/{commentID}/likes", srv.withUserAndEnv(handlers.DeleteCommentLike))
+	apiRouter.Delete("/posts/{slug}/likes", srv.withUserAndEnv(handlers.DeletePostLike))
 
 	srv.router.Mount("/api", apiRouter)
 }
