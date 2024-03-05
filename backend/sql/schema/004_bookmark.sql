65a31eccd16d9d4acc8f790ab3bd027dc6837ff2
markbussard <markbussard@outlook.com>
2024-03-04 22:59:50 -0800

    Remove updated_at timestamp column

--- backend/sql/schema/004_bookmark.sql
+++ backend/sql/schema/004_bookmark.sql
@@ -3,8 +3,7 @@ CREATE TABLE bookmark (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   user_id uuid NOT NULL REFERENCES "user"(id),
   post_id uuid NOT NULL REFERENCES post(id),
-  created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now()
+  created_at timestamp NOT NULL DEFAULT now()
 );
 
 -- +goose Down
